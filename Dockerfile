7a4d58cd053eb555c8d224edcd5ccb96b11dc9c7
alex3144 <alexboceno@hotmail.fr>
2017-07-12 13:07:03 +0200

    make it better

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+FROM semanticbus/semanticbus
--- app.js
+++ app.js
@@ -52,8 +52,8 @@ server.listen(process.env.PORT || process.env.port || process.env.OPENSHIFT_NODE
 
   // if( letsEncryptResponse != undefined && letsEncryptResponse != undefined) {
     app.get('/.well-known/acme-challenge/:challengeHash', function (req, res) {
-      var hash = req.params.challengeHash;
-      res.send('fMC11Oy02nVs-cZtJsUWGt8aHhRZe87D1eKVsFa4JmQ.rCIAnB6OZN-jvB1XIOagkbUTKQQmQ1ogeb5DUVFNUko')
+      var hash = req.params.challengeHash + ".rCIAnB6OZN-jvB1XIOagkbUTKQQmQ1ogeb5DUVFNUko";
+      res.send(hash)
     });
   // }
 
