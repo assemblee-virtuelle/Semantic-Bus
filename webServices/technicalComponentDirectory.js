378145bf24ba694e29a20b1244ab1ba89004168b
simon <simon.louvet.zen@gmail.com>
2017-07-18 17:01:05 +0200

    debut composant vlaue mapping

--- webServices/technicalComponentDirectory.js
+++ webServices/technicalComponentDirectory.js
@@ -18,6 +18,7 @@ module.exports = {
    scrapper: require('./workSpaceComponentDirectory/scrapper.js'),
    httpGet: require('./workSpaceComponentDirectory/httpGet.js'),
    sqlConnector: require('./workSpaceComponentDirectory/sqlConnecteur.js'),
+   valueMapping: require('./workSpaceComponentDirectory/valueMapping.js'),
     //  sparqlRequest: require('./workSpaceComponentDirectory/sparqlRequest.js'),
     /* some other modules you want */
     initialise : function(router,unSafeRouteur, recursivPullResolvePromise){
