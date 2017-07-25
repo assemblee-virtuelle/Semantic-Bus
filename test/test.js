ddf2dd17e044a359f59e8c6acc86228891498e30
alex3144 <alexboceno@hotmail.fr>
2017-07-25 16:02:34 +0200

    add bcryptjs

--- test/test.js
+++ test/test.js
@@ -1,26 +1,32 @@
-var supertest = require("supertest");
-var should = require("should");
+// var casper = require('casper').create();
 
-// This agent refers to PORT where the program is running.
+// casper.start('https://www.google.fr', function(){
+//     // console.log(this.getTitle)
+//     this.echo(this.getTitle(), 'INFO')
+// });
 
-var server = supertest.agent("http://localhost:8080");
+// casper.run()
 
-// UNIT test begin
 
-describe("sample dsds test",function(){
-
-  // #1 should return home page
-  it("should return login page",function(done){
-    // calling home page
-    server
-    .get("/auth/login.html")
-    .expect("Content-type",/text/)
-    .expect(200) // THis is HTTP response
-    .end(function(err,res){
-      // HTTP status should be 200
-      res.status.should.equal(200);
-      done();
+// googletesting.js
+casper.test.begin('Google search retrieves 10 or more results', 5, function suite(test) {
+    casper.start("https://www.google.fr/", function() {
+        test.assertTitle("Google", "google homepage title is the one expected");
+        test.assertExists('form[action="/search"]', "main form is found");
+        this.fill('form[action="/search"]', {
+            q: "casperjs"
+        }, true);
     });
-  });
 
+    casper.then(function() {
+        test.assertTitle("casperjs - Recherche Google", "google title is ok");
+        test.assertUrlMatch(/q=casperjs/, "search term has been submitted");
+        test.assertEval(function() {
+            return __utils__.findAll("h3.r").length >= 10;
+        }, "google search for \"casperjs\" retrieves 10 or more results");
+    });
+
+    casper.run(function() {
+        test.done();
+    });
 });
--- webServices/authWebService.js
+++ webServices/authWebService.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const moment = require('moment');
 var passport = require('passport');
 const User = require('./models/userModel');
