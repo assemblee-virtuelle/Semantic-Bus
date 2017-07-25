b29c5f5dfa83d2044dcb01e7c3e27ed264c2ea52
alex3144 <alexboceno@hotmail.fr>
2017-07-25 21:13:01 +0200

    add pic

--- test/wdio.conf.local.js
+++ test/wdio.conf.local.js
@@ -1,11 +1,11 @@
-'use strict'
-const base = require('./wdio.conf.base')
+// 'use strict'
+// const base = require('./wdio.conf.base')
 
-exports.config = Object.assign(base.config, {
-  capabilities: [
-    { browserName: 'phantomjs' }
-    // If you want to use other browsers,
-    // you may need local Selenium standalone server.
-  ],
-  services: ['phantomjs']
-})
+// exports.config = Object.assign(base.config, {
+//   capabilities: [
+//     { browserName: 'phantomjs' }
+//     // If you want to use other browsers,
+//     // you may need local Selenium standalone server.
+//   ],
+//   services: ['phantomjs']
+// })
