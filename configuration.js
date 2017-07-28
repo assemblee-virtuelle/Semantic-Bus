2fdf2eb17c98b90dd5692193e7ff2fde6951d925
alex3144 <alexboceno@hotmail.fr>
2017-07-28 19:10:27 +0200

    fix config.js

--- configuration.js
+++ configuration.js
@@ -1,8 +1,21 @@
 module.exports = {
-   mlabDBToClone : 'semantic_bus',
-   //mlabDB : 'sementic_bus_dev',
-   mlabDB : 'semantic_bus',
-   saveLock : false,
-   addLock : false,
-   https: "force"
+    mlabDBToClone: 'semantic_bus',
+    //mlabDB : 'sementic_bus_dev',
+    mlabDB: 'semantic_bus',
+    saveLock: false,
+    addLock: false,
+    secret: process.env.JWT_TOKEN,
+    https: "force",
+	configMongo: {
+		'driver': "mongoose",
+		'database': "semantic_bus",
+		'url': 'mongodb://alex:alexfoot31@ds054118.mlab.com:54118/semantic_bus',
+		'password': process.env.MLAB_PASSWORD,
+		'username': process.env.MLAB_USERNAME
+	},
+	googleAuth : {
+        'clientID'      : process.env.clientID_GOOGLE,
+        'clientSecret'  : process.env.client_GOOGLE_Secret,
+        'callbackURL'   : '/auth/'
+    }
 }
