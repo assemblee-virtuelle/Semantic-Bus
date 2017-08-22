36edb26976bb2fc2ef5ae21cc16be18bd4d14e7f
alex3144 <alexboceno@hotmail.fr>
2017-08-22 17:13:32 +0200

    fix bug

--- configuration.js
+++ configuration.js
@@ -15,8 +15,8 @@ module.exports = {
 		'username': process.env.MLAB_USERNAME
 	},
 	googleAuth : {
-        'clientID'      : process.env.clientID_GOOGLE ,
-        'clientSecret'  : process.env.client_GOOGLE_Secret,
+        'clientID'      : process.env.clientID_GOOGLE || '497545161510-jevr8h52tl51j5gsd208icp5bbbi9suq.apps.googleusercontent.com',
+        'clientSecret'  : process.env.client_GOOGLE_Secret || 'e-0uRyWiFqkbpCVWQGMh-EpW',
         'callbackURL'   : '/auth/'
     }
 }
