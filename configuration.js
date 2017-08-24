0a6dd11c44ec233c7230c2808c95b16e39f49397
alex3144 <alexboceno@hotmail.fr>
2017-08-24 10:47:13 +0200

    test ci

--- configuration.js
+++ configuration.js
@@ -7,13 +7,6 @@ module.exports = {
     secret: process.env.JWT_TOKEN || 'secret',
     https: false,
     mlab_token: 'ue_eHVRDWSW0r2YZuTLCi1BxVB_zXnOI',
-	configMongo: {
-		'driver': "mongoose",
-        'database': "semantic_bus",
-		'url': 'mongodb://alex:alexfoot31@ds052629.mlab.com:52629/sementic_bus_dev',
-		'password': process.env.MLAB_PASSWORD || 'alexfoot31',
-		'username': process.env.MLAB_USERNAME || 'alex'
-	},
 	googleAuth : {
         'clientID'      : process.env.clientID_GOOGLE || '497545161510-jevr8h52tl51j5gsd208icp5bbbi9suq.apps.googleusercontent.com',
         'clientSecret'  : process.env.client_GOOGLE_Secret || 'e-0uRyWiFqkbpCVWQGMh-EpW',
