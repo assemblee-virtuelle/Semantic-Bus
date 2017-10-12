aef073c7cc9f0268abf1462af6dc70a465fa0748
simon louvet <simon.louvet.zen@gmail.com>
2017-10-12 15:04:56 +0200

    remise en place des tests fonctionnels

--- test/wdio.conf.local.js
+++ test/wdio.conf.local.js
@@ -13,12 +13,17 @@ exports.config = Object.assign(base.config, {
   ],
   services: ['chromedriver'],
   specs: [
-    './test/e2e/test_fonctionels/connexionInscription.js',
+    //'./test/e2e/test_fonctionels/connexionInscription.js',
     './test/e2e/test_fonctionels/workspaceAndComponent.js'
 
   ],
   port: '9515',
   path: '/',
   waitforTimeout: 10000,
-  baseUrl:'http://localhost:8080/'
+  baseUrl:'http://localhost:8080/',
+  reporterOptions: {
+      junit: {
+          outputDir: './test/test_result/fonct_result'
+      }
+  }
 })
