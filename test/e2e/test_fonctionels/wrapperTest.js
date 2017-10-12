2a29e75bd17e715f62a3cdece559185274e622f8
simon louvet <simon.louvet.zen@gmail.com>
2017-10-12 15:58:43 +0200

    correction conf tes fonctionnels cloud

--- test/e2e/test_fonctionels/wrapperTest.js
+++ test/e2e/test_fonctionels/wrapperTest.js
@@ -2,7 +2,7 @@ var userModel = require('../../../lib/core/models/user_model')
 var exec = require("child_process").exec;
 var mLabPromise = require('../../../webServices/mLabPromise')
 mLabPromise.cloneDatabaseMigration().then(data => {
-    exec('wdio test/wdio.conf.cloud.fonctionnels.js',
+    exec('wdio test/wdio.conf.cloud.fonctionels.js',
     function (error, stdout, stderr) {
       console.log('stdout: ' + stdout);
       console.log('stderr: ' + stderr);
