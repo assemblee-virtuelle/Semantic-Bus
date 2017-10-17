9fa91842461a1c4936a6098052ebe1e34544c579
alex3144 <alexboceno@hotmail.fr>
2017-10-17 22:01:56 +0200

    wip

--- test/wrapperTest.js
+++ test/wrapperTest.js
@@ -1,6 +1,6 @@
-var userModel = require('../../../lib/core/models/user_model')
+var userModel = require('../lib/core/models/user_model')
 var exec = require("child_process").exec;
-var mLabPromise = require('../../../webServices/mLabPromise')
+var mLabPromise = require('../webServices/mLabPromise')
 mLabPromise.cloneDatabaseMigration().then(data => {
     exec('wdio test/wdio.conf.cloud.fonctionels.js',
     function (error, stdout, stderr) {
