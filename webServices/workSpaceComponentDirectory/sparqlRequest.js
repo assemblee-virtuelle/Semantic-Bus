06617c1682e5d3afed2834c35e0590720ccbc89a
simon louvet <simon.louvet.zen@gmail.com>
2017-11-17 05:18:57 +0100

    exception interception in sparql component

--- webServices/workSpaceComponentDirectory/sparqlRequest.js
+++ webServices/workSpaceComponentDirectory/sparqlRequest.js
@@ -2,30 +2,43 @@ module.exports = {
   type: 'Sparql request',
   description: 'Requeter en sparql sur un fichier json ld',
   editor: 'sparql-request-editor',
-  graphIcon:'sparqlRequest.png',
-  tags:[
+  graphIcon: 'sparqlRequest.png',
+  tags: [
     'http://semantic-bus.org/data/tags/middleComponents',
     'http://semantic-bus.org/data/tags/middleQueryingComponents'
   ],
   rdfstore: require('rdfstore'),
-  makeRequest: function (flowData, request) {
-  return new Promise((resolve, reject) => {
-    var query = request;
+  makeRequest: function(flowData, request) {
+    return new Promise((resolve, reject) => {
+      var query = request;
       new this.rdfstore.Store(function(err, store) {
-        store.load("application/ld+json", flowData, function(err,results) {
-          console.log(results)
-          store.execute(query, function(err, graph){
-            if(err){
-              console.log(err)
-            }else{
-              resolve({data: graph})
+        //console.log('flowData', flowData);
+        try {
+          store.load("application/ld+json", flowData, function(err, results) {
+
+            console.log(results);
+            try {
+              store.execute(query, function(err, graph) {
+                if (err) {
+                  reject(err);
+                } else {
+                  resolve({
+                    data: graph
+                  })
+                }
+              })
+            } catch (e) {
+              reject(e);
             }
           })
-        })
+
+        } catch (e) {
+          reject(e);
+        }
       });
     })
   },
-  pull: function (data, flowData) {
+  pull: function(data, flowData) {
     //console.log('REST Get JSON | pull : ',data);
     // console.log("flowDataAAAAAAAAA", flowData[0].data)
     return this.makeRequest(flowData[0].data, data.specificData.request);
