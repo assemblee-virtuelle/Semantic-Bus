2d65e3c425fbf0e4214a05c32d0edcf33e4b129c
srosset81 <srosset81@gmail.com>
2018-12-27 19:05:12 +0100

    Retry webhook post on failures

--- webServices/workSpaceComponentDirectory/webhook.js
+++ webServices/workSpaceComponentDirectory/webhook.js
@@ -26,10 +26,35 @@ module.exports = {
         return Promise.reject(new Error('Only application/json contentType is currently supported for Webhook component'));
     }
 
-    return this.fetch(componentConfig.url, {
-      method: 'POST',
-      body: body,
-      headers: {'Content-Type': componentConfig.contentType}
-    }).then(() => ({data: flowData[0].data}));
+    return this.call_webhook(componentConfig.url, {
+        method: 'POST',
+        body: body,
+        headers: {'Content-Type': componentConfig.contentType}
+    });
+  },
+
+  call_webhook: function(url, options, numRetry) {
+      if( numRetry === undefined ) numRetry = 0;
+
+      return this.fetch(url, options).catch(error => {
+          if( numRetry >= 7 ) {
+              // TODO log the failed webhook posts somewhere ?
+              console.error(error);
+          } else {
+              // Exponentially increment retry interval at every failure
+              // This will retry after 5s, 25s, 2m, 10m, 50m, 4h, 21h
+              const retryInterval = Math.pow(5, numRetry+1);
+
+              console.log(`Webhook post to ${url} failed, trying again in ${retryInterval}s...`);
+
+              return this.sleep(retryInterval * 1000).then(() => {
+                  this.call_webhook(url, options, numRetry+1);
+              });
+          }
+      });
+  },
+
+  sleep: function(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
   }
 };
