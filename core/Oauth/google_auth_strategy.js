93a44eb547d4203b19ba03443e8db8eba21fa66d
alexandre boceno <alexboceno@hotmail.fr>
2019-01-22 16:41:34 +0100

    Fix/google auth (#79)
    * hot fix
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip

--- core/Oauth/google_auth_strategy.js
+++ core/Oauth/google_auth_strategy.js
@@ -2,12 +2,11 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var UserModel = require('../models').user;
 var config = require('../../main/configuration');
 
-
 // --------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------
 
-module.exports = function(passport) {
+module.exports = (passport) => {
   passport.use(new GoogleStrategy({
       clientID: config.googleAuth.clientID,
       clientSecret: config.googleAuth.clientSecret,
@@ -16,21 +15,15 @@ module.exports = function(passport) {
       proxy: true
     },
     function(request, token, refreshToken, profile, done) {
-
       process.nextTick(function() {
-        //console.log('request',request);
         UserModel.getInstance().model.findOne({
           'googleId': profile.id
         }, function(err, user) {
-
-          //console.log("google user passeport", user)
           if (err){
             return done(err);
           }
           if (user) {
-
             user.googleToken = token
-            //console.log("google user passeport 2", user)
             UserModel.getInstance().model.findByIdAndUpdate(user._id, user, function(err, res) {
               if (err) {
                 throw err;
