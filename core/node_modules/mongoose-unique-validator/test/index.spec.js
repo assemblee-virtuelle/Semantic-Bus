8449f45cb28f94c332ce451e0c76d4557c37908e
simon louvet <smon.louvet.zen@gmail.com>
2019-01-24 19:17:48 +0100

    traca api initilise

---
+++ core/node_modules/mongoose-unique-validator/test/index.spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+// Connect
+mongoose.connect('mongodb://localhost/mongoose-unique-validator');
+mongoose.connection.on('error', function() {
+    throw new Error('Unable to connect to database.');
+});
+
+describe('Mongoose Unique Validator', function() {
+    require('./tests/validation.spec')(mongoose);
+    require('./tests/types.spec.js')(mongoose);
+    require('./tests/messages.spec')(mongoose);
+
+    after(function() {
+        mongoose.connection.db.dropDatabase();
+    });
+});
