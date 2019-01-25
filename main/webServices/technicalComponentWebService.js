0ecd89b544767ab77ef9072e20bcd11e5507e9de
alexandre boceno <alexboceno@hotmail.fr>
2019-01-25 15:54:24 +0100

    wip

--- main/webServices/technicalComponentWebService.js
+++ main/webServices/technicalComponentWebService.js
@@ -11,7 +11,7 @@ module.exports = (router, unsafeRouter, app,stompClient) => {
   const technicalComponentDirectory = require('./technicalComponentDirectory');
   technicalComponentDirectory.initialise(unsafeRouter, app, stompClient);
   
-  router.get('/technicalComponent/', function(req, res) {
+  router.get('/technicalComponent', function(req, res) {
     res.json(technicalComponentDirectory.buildDictionnaryArray());
   });
 
