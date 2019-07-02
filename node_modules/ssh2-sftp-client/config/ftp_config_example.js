5e2a73865b01c6a70e984675d556416ed49128ad
Baptiste Foucher <contact@bfoucher.fr>
2019-07-02 16:53:25 +0200

    remove node-modules

--- node_modules/ssh2-sftp-client/config/ftp_config_example.js
+++
@@ -1,8 +0,0 @@
-var privateKey = require('fs').readFileSync('/.ssh/id_rsa', {'encoding':'utf8'});
-
-module.exports = {
-    host: '127.0.0.1',
-    port: '8080',
-    username: 'test',
-    privateKey: privateKey
-};
