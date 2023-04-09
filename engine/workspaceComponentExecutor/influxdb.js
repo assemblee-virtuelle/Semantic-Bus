27b2853d651df3cb2f015629666d183a39f4d0db
Emilie Pacheco <emilie.pacheco@orange.fr>
2023-04-09 12:38:22 +0200

    Show/Hide components

--- engine/workspaceComponentExecutor/influxdb.js
+++ engine/workspaceComponentExecutor/influxdb.js
@@ -17,61 +17,125 @@ class InfluxdbConnector {
     - compression gzip requête http post -> créer composant gzip 
   */
 
-  // stringDataBuilder(jsonData,data,fieldsetString,tagString) {
-  //   // you should refer to the pages on the influxdb line protocol
-  //   // https://docs.influxdata.com/influxdb/cloud/reference/syntax/line-protocol/#naming-restrictions
+  /*
+  // OLD CODE used to send data in influxdbbucket with 2 other components HTTP consumer 
+  // (Post Request) and a Js Evaluation with an accumulator function to put 
+  // every data in the same string
+  
+  stringDataBuilder(jsonData,data,fieldsetString,tagString) {
+    // you should refer to the pages on the influxdb line protocol
+    // https://docs.influxdata.com/influxdb/cloud/reference/syntax/line-protocol/#naming-restrictions
 
-  //   // influxdb data type = string
-  //   const measurementName = data.measurement;
+    // influxdb data type = string
+    const measurementName = data.measurement;
 
-  //   const fieldSet = fieldsetString;
+    const fieldSet = fieldsetString;
 
-  //   // the timestamp needs to be in the ISO format
-  //   // like this : ,               1654940402000, the influxISO format
-  //   //     1677687240000 13
-  //   // has 6 more 0s at the end -> 1422568543702900257 19
-  //   let result;
-  //   let timestamp = '';
-  //   if(jsonData[data.timestamp]){
-  //     const date = new Date(jsonData[data.timestamp]);
-  //     if(date.toDateString().toLowerCase() !== 'invalid date'){
-  //       timestamp = date.getTime().toString();
-  //       if(timestamp.length < 19){
-  //         const nberOfZerosToAdd = 18 - timestamp.length;
-  //         for (let index = 0; index < nberOfZerosToAdd; index++) {
-  //           timestamp+="0";
-  //         }
-  //       }
-  //       result = measurementName +
-  //       // optional
-  //       tagString
-  //       // the blank space separates the measurement
-  //       // name and tag set from the field set
-  //       + " " +
-  //       fieldSet;
+    // the timestamp needs to be in the ISO format
+    // like this : ,               1654940402000, the influxISO format
+    //     1677687240000 13
+    // has 6 more 0s at the end -> 1422568543702900257 19
+    let result;
+    let timestamp = '';
+    if(jsonData[data.timestamp]){
+      const date = new Date(jsonData[data.timestamp]);
+      if(date.toDateString().toLowerCase() !== 'invalid date'){
+        timestamp = date.getTime().toString();
+        if(timestamp.length < 19){
+          const nberOfZerosToAdd = 18 - timestamp.length;
+          for (let index = 0; index < nberOfZerosToAdd; index++) {
+            timestamp+="0";
+          }
+        }
+        result = measurementName +
+        // optional
+        tagString
+        // the blank space separates the measurement
+        // name and tag set from the field set
+        + " " +
+        fieldSet;
 
-  //       if(timestamp){
-  //         result += " " + timestamp
-  //       }
-  //       // each data is a line separated by a /n
-  //       result += + " " + "\n";
-  //     }
-  //   }
-  //   return result
-  // }
+        if(timestamp){
+          result += " " + timestamp
+        }
+        // each data is a line separated by a /n
+        result += + " " + "\n";
+      }
+    }
+    return result
+  }
 
-  // fieldsetStringBuilder(jsonData,fields){
-  //   let fieldsetString = '';
-  //   for (let index = 0; index < fields.length; index++) {
-  //     const field = fields[index];
-  //     const field_value = jsonData[field];
-  //     fieldsetString += (field + "=" + field_value);
-  //     if(index != (fields.length-1)){
-  //       fieldsetString += ","
-  //     }
-  //   }
-  //   return fieldsetString
-  // }
+  fieldsetStringBuilder(jsonData,fields){
+    let fieldsetString = '';
+    for (let index = 0; index < fields.length; index++) {
+      const field = fields[index];
+      const field_value = jsonData[field];
+      fieldsetString += (field + "=" + field_value);
+      if(index != (fields.length-1)){
+        fieldsetString += ","
+      }
+    }
+    return fieldsetString
+  }
+
+  tagStringBuilder(jsonData,tags){
+    let tagString = '';
+    for (let index = 0; index < tags.length; index++) {
+      const tag = tags[index];
+      const tag_value = jsonData[tag];
+      tagString += ("," + tag + "=" + tag_value);
+    }
+    return tagString
+  }
+
+
+  // CODE IN BODY OF INFLUXDB COMPONENT ->>>>
+    const jsonData = flowData[0].data;
+    let result = '';
+    if(!(data.specificData && data.specificData.measurement)){
+      reject(new Error("Il faut fournir le nom de la mesure"))
+    }
+
+    if(!(flowData[0].data)){
+    } else {
+      const tags = this.getTags(data.specificData);
+
+      // every field entered by the user
+      const inputFields = [data.specificData.timestamp];
+      if(tags.length != 0){
+        inputFields.push(...tags);
+      }
+
+      const fields = this.getRemainingFields(jsonData,inputFields);
+
+      if(!(fields.length > 0)){
+        reject(new Error("Il faut qu'il y ait au moins un champs en entrée (field)."))
+      }
+
+      // array containing every used field :
+      const everyField = Array.from(fields);
+      everyField.push(...inputFields);
+      everyField.push(data.specificData.measurement);
+
+      everyField.forEach(element => {
+        // https://docs.influxdata.com/influxdb/v1.8/write_protocols/line_protocol_tutorial/
+        if(element && (element == "_field" || element == "measurement" || element == "time")){
+          reject(new Error("Un nom de champs s'appelle time ou _field ou _measurement."))
+        }
+      });
+
+      const fieldsetString = this.fieldsetStringBuilder(jsonData,fields);
+      const tagString = this.tagStringBuilder(jsonData,tags);
+
+      result = this.stringDataBuilder(jsonData,data.specificData,fieldsetString,tagString)
+      if(result !== undefined){
+        resolve({
+          data: result
+        })
+      }
+      else {
+        return
+      }*/
 
   // only works with integer fields !!!
   // add fields to the Point influxdb object
@@ -83,16 +147,6 @@ class InfluxdbConnector {
     }
     return point
   }
-
-  // tagStringBuilder(jsonData,tags){
-  //   let tagString = '';
-  //   for (let index = 0; index < tags.length; index++) {
-  //     const tag = tags[index];
-  //     const tag_value = jsonData[tag];
-  //     tagString += ("," + tag + "=" + tag_value);
-  //   }
-  //   return tagString
-  // }
 
   // add tags to the Point influxdb object
   addTagsToPoint(jsonData,point,tags){
@@ -131,69 +185,9 @@ class InfluxdbConnector {
     return tags
   }
 
-  // old pull code
-  /*const jsonData = flowData[0].data;
-    let result = '';
-    if(!(data.specificData && data.specificData.measurement)){
-      reject(new Error("Il faut fournir le nom de la mesure"))
-    }
-
-    if(!(flowData[0].data)){
-      // console.log('empty data here');
-    } else {
-      // console.log(flowData[0].data);
-      const tags = this.getTags(data.specificData);
-
-      // every field entered by the user
-      const inputFields = [data.specificData.timestamp];
-      if(tags.length != 0){
-        inputFields.push(...tags);
-      }
-
-      const fields = this.getRemainingFields(jsonData,inputFields);
-
-      if(!(fields.length > 0)){
-        reject(new Error("Il faut qu'il y ait au moins un champs en entrée (field)."))
-      }
-
-      // array containing every used field :
-      const everyField = Array.from(fields);
-      everyField.push(...inputFields);
-      everyField.push(data.specificData.measurement);
-      // console.log("everyfield : ",everyField.toString());
-
-      everyField.forEach(element => {
-        // console.log("first for :",element);
-        // https://docs.influxdata.com/influxdb/v1.8/write_protocols/line_protocol_tutorial/
-        if(element && (element == "_field" || element == "measurement" || element == "time")){
-          reject(new Error("Un nom de champs s'appelle time ou _field ou _measurement."))
-        }
-      });
-
-      // console.log("inputfi ", inputFields);
-      // console.log("remaining strings : ",fields);
-
-      const fieldsetString = this.fieldsetStringBuilder(jsonData,fields);
-      // console.log("before tagstring");
-      const tagString = this.tagStringBuilder(jsonData,tags);
-      // console.log("tagstring : ",tagString);
-
-      result = this.stringDataBuilder(jsonData,data.specificData,fieldsetString,tagString)
-      // console.log("result : ",result);
-      if(result !== undefined){
-        console.log(result);
-        resolve({
-          data: result
-        })
-      }
-      else {
-        return
-      }*/
-
-
   // delete every data from bucket 
   // https://github.com/influxdata/influxdb-client-js/blob/master/examples/delete.ts
-  async deleteData(influxDB,org,bucket,measurementType,location){
+  async deleteData(influxDB,org,bucket,measurementType,deleteTag,deleteTagValue){
     // console.log('*** DELETE DATA ***')
     const deleteAPI = new this.influxdbClientApi.DeleteAPI(influxDB);
     // define time interval for delete operation
@@ -207,39 +201,39 @@ class InfluxdbConnector {
         start: start.toISOString(),
         stop: stop.toISOString(),
         // see https://docs.influxdata.com/influxdb/latest/reference/syntax/delete-predicate/
-        predicate: '_measurement="'+measurementType+'" and location="'+location+'"',
+        predicate: '_measurement="'+measurementType+'" and '+deleteTag+'="'+deleteTagValue+'"',
       },
     });
   }
 
   // Execute query and receive table metadata and table row values using async iterator.
-  async iterateRows(bucket,measurementType,queryApi,querySelect) {
-    console.log('*** IterateRows ***')
+  async iterateRows(queryApi,querySelect) {
+    //console.log('*** IterateRows ***')
     // const fluxQuery = 'from(bucket:"'+bucket+'") |> range(start: -1d) |> filter(fn: (r) => r._measurement == "'+measurementType+'")';
     const fluxQuery = querySelect;
     let data = [];
-    console.log(fluxQuery);
+    // console.log(fluxQuery);
     for await (const {values, tableMeta} of queryApi.iterateRows(fluxQuery)) {
       // the following line creates a json object for each row
       const datum = tableMeta.toObject(values)
       // console.log(JSON.stringify(o, null, 2))
       data.push(datum);
     }
-    console.log('\nIterateRows SUCCESS');
+    // console.log('\nIterateRows SUCCESS');
     return data
   }
 
-  async queryGenerator(influxDB,querySelect,bucket,measurementType,org){
+  async queryGenerator(influxDB,querySelect,org){
     const queryApi = influxDB.getQueryApi(org);
     let data;
     // first if there is a query we ask it
 
     if(queryApi && querySelect){
-      console.log('entering query !');
-      console.log('bucket ',bucket);
-      console.log('measurementType : ',measurementType);
+      // console.log('entering query !');
+      // console.log('bucket ',bucket);
+      // console.log('measurementType : ',measurementType);
 
-      await this.iterateRows(bucket,measurementType,queryApi,querySelect).then( (result) => {
+      await this.iterateRows(queryApi,querySelect).then( (result) => {
         // console.log(data);
         if(!result){
           throw new Error("Aucune donnée trouvée avec la query");
@@ -272,7 +266,6 @@ class InfluxdbConnector {
           }
       }
 
-      // test -> if location is empty we do not delete the datas
       let insertData = true;
       // checking that the data for the tags isn't empty or undefined
       tags.forEach(tag => {
@@ -290,7 +283,7 @@ class InfluxdbConnector {
         const point2 = this.addTagsToPoint(jsonData,point1,tags)
         const point3 = this.addFieldsToPoint(jsonData,point2,fields);
 
-        console.log(point3);
+        // console.log(point3);
       
         // then we write the data
         
@@ -324,6 +317,9 @@ class InfluxdbConnector {
             reject(new Error("Il faut fournir le nom de la mesure"))
           }
 
+          const choice = data.specificData.choice;
+          // console.log('CHOICE MADE : ',choice);
+
           const url = data.specificData.url;
           const token = data.specificData.apiKey;
           const org = data.specificData.organization;
@@ -332,57 +328,62 @@ class InfluxdbConnector {
           const timestamp = data.specificData.timestamp;
           const querySelect = data.specificData.querySelect;
 
-          // TODO when removal of data we have to choose a field ! location temporary
-          const location = flowData ? flowData[0].data['location'] : '';
-          // console.log('location : ',location);
-
-          let deleteData = !(data.specificData.notErase);
-
           // creation of the communication interface for influxdb
           const influxDB = new this.influxdbClient.InfluxDB({url, token});
           
-          // we delete everything in the bucket from now to year 2000
-          // we either delete or write the data ???
-          if(deleteData){
-            console.log('deletedata');
-            this.deleteData(influxDB,org,bucket,measurementType,location)
-              .then(async () => {
-                console.log('\nSuppression des données');
-                resolve({"data" : 'Suppression des données réalisée'});
-              })
-              .catch((error) => {
-                console.error(error);
-                reject(new Error('Suppression des données non réalisée'));
-                // console.log('\nFinished ERROR')
-              });
-          } else if(querySelect) {
-            console.log('querySelect');
-            await this.queryGenerator(influxDB,querySelect,bucket,measurementType,org).then((result) => {
-              console.log('data : ',result);
+          switch (choice) {
+            case "inserer":
+              // console.log('writedata');
+              const jsonData = flowData[0].data;
+              const tags = this.getTags(data.specificData);
+
+              // every field entered by the user
+              const inputFields = [data.specificData.timestamp];
+              if(tags.length > 0){
+                inputFields.push(...tags);
+              }
+
+              // gets the fields that weren't entered by the user
+              const fields = this.getRemainingFields(jsonData,inputFields);
+              const result = this.writeData(jsonData,influxDB,timestamp,fields,tags,org,bucket,measurementType);
+              // console.log('donnée insérée ');
               resolve({'data' : result})
-            }).catch( (e) => {
-              console.log(e);
-              reject(new Error(e));
-            })
-          } else {
-            console.log('writedata');
-            const jsonData = flowData[0].data;
+              break;
 
-            const tags = this.getTags(data.specificData);
+            case "supprimer":
+              // console.log('deletedata');
 
-            // every field entered by the user
-            const inputFields = [data.specificData.timestamp];
-            if(tags.length > 0){
-              inputFields.push(...tags);
-            }
+              // we delete everything in the bucket from now to year 2000
+              const deleteTagValue = data.specificData.deleteTagValue;
+              const deleteTag = data.specificData.deleteTag;
 
-             // gets the fields that weren't entered by the user
-            const fields = this.getRemainingFields(jsonData,inputFields);
+              this.deleteData(influxDB,org,bucket,measurementType,deleteTag,deleteTagValue)
+                .then(async () => {
+                  // console.log('\nSuppression des données');
+                  resolve({"data" : 'Suppression des données réalisée'});
+                })
+                .catch((error) => {
+                  // console.error(error);
+                  reject(new Error('Suppression des données non réalisée'));
+                  // console.log('\nFinished ERROR')
+                });
+              break;
 
-            const result = this.writeData(jsonData,influxDB,timestamp,fields,tags,org,bucket,measurementType);
-            console.log('donnée insérée ');
-            resolve({'data' : result})
+            case "requeter":
+              // console.log('querySelect');
+              await this.queryGenerator(influxDB,querySelect,org).then((result) => {
+                // console.log('data : ',result);
+                resolve({'data' : result})
+              }).catch( (e) => {
+                // console.log(e);
+                reject(new Error(e));
+              })
+              break;
+
+            default:
+              break;
           }
+
         } catch(e) {
         reject(e)
       }
--- main/client/static/tag/editorComponents/bdd/influxdb-connector-editor.tag
+++ main/client/static/tag/editorComponents/bdd/influxdb-connector-editor.tag
@@ -14,6 +14,19 @@
     <div class="bar"/>
   </div>
   <!-- Champ du composant -->
+  <!--  Formulaire de choix lie au composant  -->
+  <form action='' ref="actionToggle" id="actionToggle">
+    <p>Choix de l'action à réaliser : </p>
+    <input type="radio" id="inserer" name="action" value="inserer" ref="insertChecked" onchange={saveRadioState} checked={data.specificData.insertChecked}>
+    <label for="inserer">Insérer des données</label><br>
+    <input type="radio" id="requeter" name="action" value="requeter" ref="requestChecked" onchange={saveRadioState} checked={data.specificData.requestChecked}>
+    <label for="requeter">Requêter des données</label><br>
+    <input type="radio" id="supprimer" name="action" value="supprimer" ref="deleteChecked" onchange={saveRadioState} checked={data.specificData.deleteChecked}>
+    <label for="supprimer">Supprimer des données</label>
+  </form>
+  <div>
+    <div class="bar"/>
+  </div>
   <!--  Url influxdb  -->
   <label class="labelFormStandard">Url Influxdb:</label>
   <div class="cardInput">
@@ -29,52 +42,64 @@
   <div class="cardInput">
     <input class="inputComponents" type="text" ref="organization" value={data.specificData.organization} placeholder="Data-Players" onchange={organizationChange}/>
   </div>
-  <!--  Bucket  -->
-   <label class="labelFormStandard">Bucket:</label>
-  <div class="cardInput">
-    <input class="inputComponents" type="text" ref="bucket" value={data.specificData.bucket} placeholder="Test" onchange={bucketChange}/>
-  </div>
-  <!--  Nom de la mesure  -->
-  <label class="labelFormStandard">Nom de la mesure:</label>
-  <div class="cardInput">
-    <input class="inputComponents" type="text" ref="measurement" value={data.specificData.measurement} placeholder="electricitySensors" onchange={measurementChange}/>
-  </div>
-  <!--  Timestamp  -->
-  <label class="labelFormStandard">Champs contenant le timestamp:</label>
-  <div class="cardInput">
-    <input class="inputComponents" type="text" ref="timestamp" value={data.specificData.timestamp} onchange={timestampChange}/>
-  </div>
-  <!--  Suppression des données  -->
-  <div class="options">
-    <label class="labelFormStandard">Ne pas supprimer avant injection</label>
+  <!--  Elements nécessaires pour suppression ou insertion  -->
+    <!--  Bucket  -->
+  <div id= "bucketMeasureType" style='display:none;'>
+    <div id="bucket">
+      <label class="labelFormStandard">Bucket:</label>
+      <div class="cardInput">
+        <input class="inputComponents" type="text" ref="bucket" value={data.specificData.bucket} placeholder="Test" onchange={bucketChange}/>
+      </div>
+    </div>
+    <!--  Nom de la mesure  -->
+    <label class="labelFormStandard">Nom de la mesure:</label>
     <div class="cardInput">
-      <label class="switch">
-          <input type="checkbox" name="notEraseValueInput" ref="notEraseValueInput" checked={data.specificData.notErase} onchange={notEraseChange}/>
-          <span class="slider round"></span>
-      </label>
+      <input class="inputComponents" type="text" ref="measurement" value={data.specificData.measurement} placeholder="electricitySensors" onchange={measurementChange}/>
     </div>
   </div>
-    <!--  Query Infludb  -->
-  <label>Valeur(Mettre uniquement query de recherche de données):
-    <a href="https://docs.influxdata.com/influxdb/v2.6/reference/syntax/influxql/spec/" target="_blank"><img src="./image/help.png" alt="Aide" width="25px" height="25px"></a>
-  </label>
-  <div class="containerH">
-    <textarea class="textArea" placeholder="from(bucket:'Test') |> range(start: -3d) |> filter(fn: (r) => r._measurement == 'electricitySensors')" type="textarea" ref="querySelect" onchange={querySelectChange} value={data.specificData.querySelect} style="flex-grow:1">{data.specificData.querySelect}</textarea>
+  <!--  Insertion des données  -->
+  <div id="insertData" show={ data.specificData.insertChecked == 'checked' }>
+    <!--  Timestamp  -->
+    <label class="labelFormStandard">Champs contenant le timestamp:</label>
+    <div class="cardInput">
+      <input class="inputComponents" type="text" ref="timestamp" value={data.specificData.timestamp} onchange={timestampChange}/>
+    </div>
+    <!--  Champs d'id  -->
+    <label class="labelFormStandard">Champs d'id:</label>
+    <div class="cardInput">
+      <div onclick={addRowClick} class="btnFil commandButtonImage">
+        Ajouter
+        <img class="imgFil" src="./image/ajout_composant.svg" title="Importer un Workflow">
+        <input onchange={import} ref="import" type="file" style="display:none;"/>
+      </div>
+    </div>
+    <zentable ref="tagsTable" title="tag(s) a choisir" allowdirectedit={true} disallowselect={true} disallownavigation={true}>
+      <yield to="row">
+        <input placeholder="Tag" type="text" style="flex-basis:90%; margin: 5px" value={tag} data-field="tag"/>
+      </yield>
+    </zentable>
   </div>
-  <!--  Champs d'id  -->
-  <label class="labelFormStandard">Champs d'id:</label>
-  <div class="cardInput">
-    <div onclick={addRowClick} class="btnFil commandButtonImage">
-      Ajouter
-      <img class="imgFil" src="./image/ajout_composant.svg" title="Importer un Workflow">
-      <input onchange={import} ref="import" type="file" style="display:none;"/>
+  <!--  Requetage des données  -->
+  <div id="queryData" show={ data.specificData.requestChecked == 'checked' }>
+    <label>Valeur(Mettre uniquement query de recherche de données):
+      <a href="https://docs.influxdata.com/influxdb/v2.6/reference/syntax/influxql/spec/" target="_blank"><img src="./image/help.png" alt="Aide" width="25px" height="25px"></a>
+    </label>
+    <div class="containerH">
+      <textarea class="textArea" placeholder="from(bucket:'Test') |> range(start: -3d) |> filter(fn: (r) => r._measurement == 'electricitySensors')" type="textarea" ref="querySelect" onchange={querySelectChange} value={data.specificData.querySelect} style="flex-grow:1">{data.specificData.querySelect}</textarea>
     </div>
   </div>
-  <zentable ref="tagsTable" title="tag(s) a choisir" allowdirectedit={true} disallowselect={true} disallownavigation={true}>
-    <yield to="row">
-      <input placeholder="Tag" type="text" style="flex-basis:90%; margin: 5px" value={tag} data-field="tag"/>
-    </yield>
-  </zentable>
+    <!--  Suppression des données  -->
+  <div id="deleteData" show={ data.specificData.deleteChecked == 'checked' }>
+    <label class="labelFormStandard">Tag utilisé pour la suppression:</label>
+    <div class="cardInput">
+      <input class="inputComponents" type="text" ref="deleteTag" value={data.specificData.deleteTag} placeholder="location" onchange={deleteTagChange}/>
+    </div>
+    <label class="labelFormStandard">Valeur du tag en question:</label>
+    <div class="cardInput">
+      <input class="inputComponents" type="text" ref="deleteTagValue" value={data.specificData.deleteTagValue} placeholder="coma" onchange={deleteTagValueChange}/>
+    </div>
+  </div>
+
 
 <script>
 
@@ -88,7 +113,40 @@
   this.data.specificData.bucket = '';
   this.data.specificData.organization = '';
   this.data.specificData.querySelect = '';
+  this.data.specificData.choice = '';
+  this.data.specificData.deleteTag = '';
+  this.data.specificData.deleteTagValue = '';
   this.data.specificData.tags = [];
+
+  this.data.specificData.insertChecked = '';
+  this.data.specificData.deleteChecked = '';
+  this.data.specificData.requestChecked = '';
+
+  //code for the show/hide each component part
+  showHideElements(checkedRadioValue) {
+    if (checkedRadioValue == "requeter") {
+      this.data.specificData.insertChecked = '';
+      this.data.specificData.deleteChecked = '';
+      this.data.specificData.requestChecked = 'checked';
+    } else if (checkedRadioValue == "supprimer") {
+      this.data.specificData.insertChecked = '';
+      this.data.specificData.deleteChecked = 'checked';
+      this.data.specificData.requestChecked = '';
+    } else if (checkedRadioValue == "inserer") {
+      this.data.specificData.insertChecked = 'checked';
+      this.data.specificData.deleteChecked = '';
+      this.data.specificData.requestChecked = '';
+    }
+  }
+
+  saveRadioState(){
+    console.log('LOL', 'saveRadioState');
+    this.checkedRadio = document.querySelector('input[name="action"]:checked')
+    if (this.checkedRadio) {
+      this.showHideElements(this.checkedRadio.value);
+    }
+  }
+  //end of the code for show/hide elements
 
   //beggining of code for the input section
   addRowClick(e) {
@@ -96,6 +154,12 @@
   }
   //end of code for the input section
 
+  deleteTagValueChange(e){
+    this.data.specificData.deleteTagValue = e.target.value;
+  }
+  deleteTagChange(e) {
+    this.data.specificData.deleteTag = e.target.value;
+  }
   measurementChange(e) {
     this.data.specificData.measurement = e.target.value;
   }
@@ -117,8 +181,8 @@
   organizationChange(e) {
     this.data.specificData.organization = e.target.value;
   }
-  notEraseChange(e){
-    this.data.specificData.notErase = e.target.checked
+  deleteChange(e){
+    this.data.specificData.deleteData = e.target.checked
   }
   querySelectChange(e){
     this.data.specificData.querySelect = e.target.value;
@@ -134,8 +198,14 @@
     this.refs.bucket.data = this.data.specificData.bucket;
     this.refs.organization.data = this.data.specificData.organization;
     this.refs.tagsTable.data = this.data.specificData.tags || [];
-    this.refs.notErase = this.data.specificData.notErase || false;
+    this.refs.deleteData = this.data.specificData.deleteData || false;
     this.refs.querySelect = this.data.specificData.querySelect;
+    this.refs.choice = this.data.specificData.choice;
+    this.refs.deleteTag = this.data.specificData.deleteTag;
+    this.refs.deleteTagValue = this.data.specificData.deleteTagValue;
+    this.refs.insertChecked = this.data.specificData.insertChecked;
+    this.refs.deleteChecked = this.data.specificData.deleteChecked;
+    this.refs.requestChecked = this.data.specificData.requestChecked;
     this.update();
   }.bind(this);
 
@@ -148,6 +218,7 @@
       });
     RiotControl.on('item_current_changed',this.updateData);
   });
+  
   this.on('unmount', function () {
     RiotControl.off('item_current_changed', this.updateData);
   });
