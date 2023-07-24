256aabc1505deb2f6ae78f8d050a3c6038117a4a
simon louvet <simon.louvet.pro@gmail.com>
2023-07-25 00:52:43 +0200

    fix geocode fr bug

--- engine/workspaceComponentExecutor/gouvFrGeoLocaliser.js
+++ engine/workspaceComponentExecutor/gouvFrGeoLocaliser.js
@@ -8,31 +8,55 @@ class GouvFrGeoLocaliser {
   }
 
   async geoLocalise2(flowData, specificData) {
-      if(Array.isArray(flowData)){
-        throw new Error('input data can not be an array');
+      try {
+        if (flowData==undefined){
+          throw new Error("input data have to exist")
+        }
+        if(Array.isArray(flowData)){
+          throw new Error('input data can not be an array');
+        }
+        if(typeof flowData === 'string' || flowData instanceof String){
+          throw new Error('input data have to be an object');
+        }
+        var address = {
+          street: flowData[specificData.streetPath],
+          town: flowData[specificData.townPath],
+          postalCode: flowData[specificData.postalCodePath],
+          country: flowData[specificData.countryPath]
+        }
+        let adressArray = [];
+        if(address.street){
+          adressArray.push(address.street);
+        }
+        if(address.postalCode){
+          adressArray.push(address.postalCode);
+        }
+        if(address.town){
+          adressArray.push(address.town);
+        }
+        if(address.country){
+          adressArray.push(address.country);
+        }
+        var addressGouvFrFormated = adressArray.join(',');
+        const geoResponse = await fetch(`http://api-adresse.data.gouv.fr/search/?q=${addressGouvFrFormated}`);
+        const geoResponseObject = await geoResponse.json();
+        flowData[specificData.latitudePath] = geoResponseObject.features[0].geometry.coordinates[1];
+        flowData[specificData.longitudePath] = geoResponseObject.features[0].geometry.coordinates[0];
+        
+        console.log(flowData)
+        return {
+          data: flowData
+        };
+      } catch (error) {
+        console.error(e);
+        throw e
       }
-      if(typeof flowData === 'string' || flowData instanceof String){
-        throw new Error('input data have to be an object');
-      }
-      var address = {
-        street: flowData[specificData.streetPath],
-        town: flowData[specificData.townPath],
-        postalCode: flowData[specificData.postalCodePath],
-        country: flowData[specificData.countryPath]
-      }
-      var addressGouvFrFormated = [address.street, address.postalCode, address.town, address.country].join(',');
-      const geoResponse = await fetch(`http://api-adresse.data.gouv.fr/search/?q=${addressGouvFrFormated}`);
-      const geoResponseObject = await geoResponse.json();
-      flowData[specificData.latitudePath] = geoResponseObject.features[0].geometry.coordinates[1];
-      flowData[specificData.longitudePath] = geoResponseObject.features[0].geometry.coordinates[0];
 
-      return {
-        data: flowData
-      };
   }
 
   pull(data, flowData) {
-    return this.geoLocalise2(flowData[0].data, data.specificData)
+    // console.log('flowData',flowData[0])
+    return this.geoLocalise2(flowData?flowData[0].data:undefined, data.specificData)
   }
 }
 
