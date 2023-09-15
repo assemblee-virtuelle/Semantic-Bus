77e119e8d8e335a7f64c93d1d21bb7b0c5b15c8a
epacheco <emiliepacheco@yahoo.com>
2023-09-15 19:35:22 +0200

    Upgrades on the script

--- scripts/create-component.sh
+++ scripts/create-component.sh
@@ -11,6 +11,7 @@ echo "Entering Tests"
     # Faire le code du composant ici   
 
 className="split"
+classNameMaj="Split"
 editorName="split-editor"
 classRegex="---CLASSNAME---"
 editorRegex="---EDITORNAME---"
@@ -22,33 +23,42 @@ rm ./filemodifications/*
 # création de fichiers ->>>>
 # engine/workspaceComponentExecutor/CLASS.js tagfile
 cp ./squeletton/engineClass.txt ./filemodifications/engineClass.txt
-mv ./filemodifications/engineClass.txt ./filemodifications/engine$className.js
 # nom de la classe qui change ---CLASS---
 # /!\ nom du fichier à changer ---CLASS---.js
 
 # lecture du fichier ./filemodifications/engine$className.js + regex
 # on remplace les ---CLASS--- par le nom de la classe
-sed -i "s/$classRegex/$className/" filemodifications/engine$className.js
+sed -i "s/$classRegex/$classNameMaj/" ./filemodifications/engineClass.txt
+
+mv ./filemodifications/engineClass.txt ../engine/workspaceComponentExecutor/$className.js
+
+echo "First file modified"
 
 # main/client/static/tag/editorComponents/transformation/CLASS-editor.tag
 # balises de tag qui changent ---EDITORNAME---
 # nom du fichier wiki qui change ---CLASS---
 # nom du fichier qui changent ---EDITORNAME---
 cp ./squeletton/mainTag.txt ./filemodifications/mainTag.txt
-mv ./filemodifications/mainTag.txt ./filemodifications/$editorName.tag
 
-sed -i "s/$classRegex/$className/" ./filemodifications/$editorName.tag
-sed -i "s/$editorRegex/$editorName/" ./filemodifications/$editorName.tag
+sed -i "s/$classRegex/$className/" ./filemodifications/mainTag.txt
+sed -i "s/$editorRegex/$editorName/" ./filemodifications/mainTag.txt
 
-# main/server/workspaceComponentInitialize/jsEvaluation.js
+mv ./filemodifications/mainTag.txt ../main/client/static/tag/editorComponents/transformation/$editorName.tag
+
+echo "2nd file modified"
+
+# main/server/workspaceComponentInitialize/CLASS.js
 cp ./squeletton/mainClass.txt ./filemodifications/mainClass.txt
-mv ./filemodifications/mainClass.txt ./filemodifications/$className.js
 # nom de la classe à changer ---CLASS---
 # nom du titre du composant à changer ---CLASS---
 # nom de l'éditeur à changer ---EDITORNAME---
 
-sed -i "s/$classRegex/$className/" ./filemodifications/$className.js
-sed -i "s/$editorRegex/$editorName/" ./filemodifications/$className.js
+sed -i "s/$classRegex/$classNameMaj/" ./filemodifications/mainClass.txt
+sed -i "s/$editorRegex/$editorName/" ./filemodifications/mainClass.txt
+
+mv ./filemodifications/mainClass.txt ../main/server/workspaceComponentInitialize/$className.js
+
+echo "3rd file modified"
 
 # ajout des lignes dans les fichiers ->>>>
 # main/server/services/technicalComponentDirectory.js
@@ -78,5 +88,16 @@ echo "timer config text : $configText"
 
 # engine/configuration.js
 echo "engine config text : $configText"
+
+# TODONEXT -> 
+# FICHIERS DE CONFIGURATION à modifer !!!!!
+# arguments de script
+# rajouter variables pour noms de fichiers
+# test erreurs
+# prévoir d'autres choses comme description composant à personnaliser, 
+# choisir dossier du composant avec un switch case en fonction de son utilité -> transfo, entrées/sorties
+
+# on supprime tout ce qui est dans fileModifications après
+# rm ./filemodifications/*
 
 echo "End"
