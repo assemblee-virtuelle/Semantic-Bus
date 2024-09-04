0aa0fdde17fd741ca8e874b6eb3808136f1d2123
simon louvet <simon.louvet.pro@gmail.com>
2024-09-04 17:37:21 +0200

    bug fix flow sort in agregator

--- engine/workspaceComponentExecutor/simpleAgregator.js
+++ engine/workspaceComponentExecutor/simpleAgregator.js
@@ -2,44 +2,46 @@ const fragment_lib = require('../../core/lib/fragment_lib_scylla');
 
 'use strict';
 class SimpleAgregator {
-  constructor () {
+  constructor() {
   }
   async getPrimaryFlow(data, dataFlow) {
-    // Tri des dataFlow par la propriété _id
-    dataFlow.sort((a, b) => a.componentId - b.componentId);
-    console.log('____________dataFlow',dataFlow);
+    dataFlow.sort((a, b) => {
+      let componentIdA = a.componentId.toString();
+      let componentIdB = b.componentId.toString();
+      return componentIdA.localeCompare(componentIdB);
+    });
 
     const newRooFrag = await fragment_lib.createRootArrayFragFromFrags(dataFlow.map(df => df.fragment))
-    
+
     // console.log('____________newRooFrag',newRooFrag)
-    
+
     return {
-        fragment:newRooFrag.id
-      }
+      fragment: newRooFrag.id
+    }
   }
   async getSecondaryFlow(data, flowData) {
     return []
   }
-  pull (data, flowData) {
+  pull(data, flowData) {
     return new Promise((resolve, reject) => {
       // console.log('___',flowData)
       var resultFlow = []
       for (let flow of flowData) {
-        if(Array.isArray(flow.data)){
-          for(let originData of flow.data){
+        if (Array.isArray(flow.data)) {
+          for (let originData of flow.data) {
             // if (Array.isArray(originData)) {
             //   for (let record of originData) {
             //     resultFlow.push(record)
             //   }
             // } else {
-              resultFlow.push(originData)
+            resultFlow.push(originData)
             // }
           }
-        } else{
+        } else {
           resultFlow.push(flow.data)
         }
       }
-      resultFlow=resultFlow.flat();
+      resultFlow = resultFlow.flat();
       resolve({
         data: resultFlow
       })
