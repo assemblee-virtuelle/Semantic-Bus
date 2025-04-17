83c3c97321665a52e32e24ecaf3a383cc85a5ed7
simon louvet <simon.louvet.pro@gmail.com>
2025-04-17 04:42:30 +0200

    patch flat when array are not frags

--- engine/workspaceComponentExecutor/flat.js
+++ engine/workspaceComponentExecutor/flat.js
@@ -2,7 +2,7 @@
 const fragmentModel = require('../../core/models/fragments_model_scylla.js');
 
 class Flat {
-  constructor () {
+  constructor() {
     this.fragment_lib = require('../../core/lib/fragment_lib_scylla.js');
   }
 
@@ -12,38 +12,47 @@ class Flat {
         // Get the input fragment and dfob
         const inputFragment = flowData[0]?.fragment;
         const inputDfob = flowData[0]?.dfob;
-        
+
         if (!inputFragment) {
           resolve();
           return;
         }
 
         // Create a new array fragment to store the flattened results
-        let resultFragment = await this.fragment_lib.createArrayFrag(undefined,true);
+        let resultFragment = await this.fragment_lib.createArrayFrag(undefined, true);
         let index = 1;
 
-        const isArrayFrag = inputFragment.branchFrag!=undefined;
+        const isArrayFrag = inputFragment.branchFrag != undefined;
         if (isArrayFrag) {
-          const arayChildren = await fragmentModel.searchFragmentByField({ 
+          // console.log('inputFragment is arrayFrag', inputFragment)
+          const arrayChildren = await fragmentModel.searchFragmentByField({
             branchOriginFrag: inputFragment.branchFrag
           }, {
             index: 'ASC'
           });
-          for (const child of arayChildren) {
-            const isChildArrayFrag = child.branchFrag!=undefined;
+          // console.log('arrayChildren', arrayChildren)
+          for (const child of arrayChildren) {
+            const isChildArrayFrag = child.branchFrag != undefined;
             if (isChildArrayFrag) {
               const childrenOfChild = await fragmentModel.searchFragmentByField({
                 branchOriginFrag: child.branchFrag
               }, {
-              index: 'ASC'
-            });
-            for (const childOfChild of childrenOfChild) {
+                index: 'ASC'
+              });
+              for (const childOfChild of childrenOfChild) {
+                // console.log('adding child by fragment', childOfChild)
                 await this.fragment_lib.addFragToArrayFrag(childOfChild, resultFragment, index);
                 index++;
               }
+            }else{
+              for(let i = 0; i < child.data.length; i++){
+                // console.log('adding child by data', child.data[i])
+                await this.fragment_lib.addDataToArrayFrag(child.data[i], resultFragment, index);
+                index++;  
+              }
             }
           }
-        } 
+        }
 
         resultFragment.id = inputFragment.id;
         await fragmentModel.updateFragment(resultFragment);
@@ -55,21 +64,21 @@ class Flat {
     });
   }
 
-  pull (data, flowData, pullParams) {
+  pull(data, flowData, pullParams) {
     return new Promise((resolve, reject) => {
       try {
         var result = []
-        if (Array.isArray(flowData[0].data)){
+        if (Array.isArray(flowData[0].data)) {
           result = flowData[0].data.flat();
         }
-        else{
+        else {
           throw new Error("Data are not in an array structure.")
         }
         resolve({
           data: result
         })
       } catch (e) {
-        reject (e)
+        reject(e)
       }
     })
   }
